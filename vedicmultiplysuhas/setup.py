e44144dc931f475d9ad79a49dfaedcbd7c9c0fe0
suhas Gaikwad <suhassureshgaikwad@gmail.com>
2019-12-19 23:33:21 +0530

    license and setup added

---
+++ vedicmultiplysuhas/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+setuptools.setup(
+
+     name='vedicmultiplysuhas',
+
+     version='0.1',
+
+     scripts=['vedicmultiplysuhas'] ,
+
+     author="Suhas Gaikwad",
+
+     author_email="suhassureshgaikwad@gmail.com",
+
+     description="A  utility package",
+
+     long_description="long_description",
+
+     long_description_content_type="text/markdown",
+
+     url="https://github.com/suhasgaikwad8902/first_package",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+)
